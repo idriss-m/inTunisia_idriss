8423599efae77e9b8dc009ad65b497e5c3b347e2
Idriss Mezguich SST <idriss.mezguich@gmail.com>
2020-07-03 09:51:40 +0200

    adding acceptance env

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -32,8 +32,13 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            
             $bundles[] = new Csa\Bundle\GuzzleBundle\CsaGuzzleBundle();
+        }
+
+        if ('dev' === $this->getEnvironment()) {
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
         }
 
         return $bundles;
